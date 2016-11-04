f6bd2e07782a965b0437bf4b3ba87a2d5327318d
Naoyoshi Kobayashi <kobayashi@wacul.co.jp>
2016-11-04 16:21:33 +0900

    update columns.json

--- generate_files.go
+++ generate_files.go
@@ -1,4 +1,4 @@
 package gasegment
 
-//go:generate curl https://www.googleapis.com/analytics/v3/metadata/ga/columns -o files/columns.json
+//go:generate sh -c "curl https://www.googleapis.com/analytics/v3/metadata/ga/columns | jq -S . > files/columns.json"
 //go:generate go-bindata -ignore="\.DS_Store" -o asset/asset.go -pkg asset -prefix files files/...

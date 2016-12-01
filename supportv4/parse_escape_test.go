10ab6a6d8a856a26b938757feb0692065a69aa7b
podhmo <ababjam61@gmail.com>
2016-12-01 14:18:53 +0900

    InListOp support (v3 -> v4) (#10)
    * support inlist operator
    
    * backquote

---
+++ supportv4/parse_escape_test.go
@@ -0,0 +1,29 @@
+package supportv4
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestParseInListValue(t *testing.T) {
+	candidates := []struct {
+		value    string
+		expected []string
+	}{
+		{value: `foo bar`, expected: []string{"foo bar"}},
+		{value: `foo|bar`, expected: []string{"foo", "bar"}},
+		{value: `foo\|bar`, expected: []string{"foo|bar"}},
+		{value: `foo\\|bar`, expected: []string{"foo\\", "bar"}},
+		{value: `foo\a\|bar`, expected: []string{"fooa|bar"}},
+	}
+
+	for _, c := range candidates {
+		t.Run(fmt.Sprintf("parseInList %s", c.value), func(t *testing.T) {
+			parsed := ParseStringWithEscape(c.value, '|', '\\')
+			if !reflect.DeepEqual(c.expected, parsed) {
+				t.Errorf("expected %v, but %v", c.expected, parsed)
+			}
+		})
+	}
+}

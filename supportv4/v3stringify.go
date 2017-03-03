f39a1e4ea23dc322c1e4c93423521e35afdbf165
Naoyoshi Kobayashi <kobayashi@wacul.co.jp>
2017-03-03 14:16:11 +0900

    user session

--- supportv4/v3stringify.go
+++ supportv4/v3stringify.go
@@ -10,8 +10,89 @@ import (
 
 // V4[AST] -> V3[string]
 
+// V3StringifyDynamicSegment :
+func V3StringifyDynamicSegment(node *gapi.DynamicSegment) (string, error) {
+	if node.SessionSegment != nil {
+		inner, err := V3StringifySegmentDefinition(node.SessionSegment)
+		if err != nil {
+			return "", err
+		}
+		return "sessions::" + inner, nil
+	}
+	if node.UserSegment != nil {
+		inner, err := V3StringifySegmentDefinition(node.UserSegment)
+		if err != nil {
+			return "", err
+		}
+		return "users::" + inner, nil
+	}
+	return "", errors.New("at least either a session or a user segment")
+}
+
+// V3StringifySegmentDefinition :
+func V3StringifySegmentDefinition(node *gapi.SegmentDefinition) (string, error) {
+	if node == nil {
+		return "", nil
+	}
+	outer := make([]string, 0, len(node.SegmentFilters))
+	for _, segmentFilter := range node.SegmentFilters {
+		inner, err := V3StringifySegmentFilter(segmentFilter)
+		if err != nil {
+			return "", err
+		}
+		outer = append(outer, inner)
+	}
+	return strings.Join(outer, ";"), nil
+}
+
+// V3StringifySegmentFilter :
+func V3StringifySegmentFilter(node *gapi.SegmentFilter) (string, error) {
+	if node == nil {
+		return "", nil
+	}
+	if node.SimpleSegment != nil {
+		inner, err := V3StringifySimpleSegment(node.SimpleSegment)
+		if err != nil {
+			return "", err
+		}
+		if node.Not {
+			inner = "!" + inner
+		}
+		return "conditions::" + inner, nil
+	}
+	if node.SequenceSegment != nil {
+		return "", errors.New("TODO")
+	}
+	return "", errors.New("at least either a simple or a sequence segment")
+}
+
+// V3StringifySimpleSegment :
+func V3StringifySimpleSegment(node *gapi.SimpleSegment) (string, error) {
+	if node == nil {
+		return "", nil
+	}
+	outer := make([]string, 0, len(node.OrFiltersForSegment))
+	for _, orFilter := range node.OrFiltersForSegment {
+		inner := make([]string, 0, len(orFilter.SegmentFilterClauses))
+		for _, segmentFilter := range orFilter.SegmentFilterClauses {
+			expression, err := V3StringifySegmentFilterClause(segmentFilter)
+			if err != nil {
+				return "", err
+			}
+			inner = append(inner, expression)
+		}
+		if len(inner) > 0 {
+			outer = append(outer, strings.Join(inner, ","))
+		}
+	}
+	return strings.Join(outer, ";"), nil
+}
+
 // V3StringifySegmentFilterClause :
 func V3StringifySegmentFilterClause(node *gapi.SegmentFilterClause) (string, error) {
+	if node == nil {
+		return "", nil
+	}
 	if node.DimensionFilter != nil {
 		return V3StringifySegmentDimensionFilter(node.DimensionFilter, node.Not)
 	}
